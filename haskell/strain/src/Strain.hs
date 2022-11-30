cdfa89d15268714167885dc94c67ceb6bd22d67f
reixn <reixn@outlook.com>
2022-11-30 13:21:13 +0800

    [haskell] finish strain

---
+++ haskell/strain/src/Strain.hs
@@ -0,0 +1,8 @@
+module Strain (keep, discard) where
+
+discard :: (a -> Bool) -> [a] -> [a]
+discard _ [] = []
+discard p (x : xs) = if p x then discard p xs else x : discard p xs
+
+keep :: (a -> Bool) -> [a] -> [a]
+keep p = discard (not . p)
